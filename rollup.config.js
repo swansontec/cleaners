35462fd69cd6c0f5c236ec82401e1972a242645a
William Swanson <swansontec@gmail.com>
2020-08-26 17:05:39 -0700

    Expose a module entry point for Node 14+

--- rollup.config.js
+++ rollup.config.js
@@ -5,5 +5,25 @@ import packageJson from './package.json'
 export default {
   input: 'src/index.js',
   output: { file: packageJson.main, format: 'cjs', sourcemap: true },
-  plugins: [filesize()]
+  plugins: [mjs(), filesize()]
+}
+
+/**
+ * Tiny plugin to generate .mjs wrappers for each entry point.
+ */
+function mjs() {
+  return {
+    name: 'rollup-plugin-mjs',
+    generateBundle(options, bundle) {
+      for (const fileName of Object.keys(bundle)) {
+        const chunk = bundle[fileName]
+        if (chunk.type !== 'chunk' || !chunk.isEntry) continue
+        this.emitFile({
+          type: 'asset',
+          fileName: fileName.replace(/js$/, 'mjs'),
+          source: `export { ${chunk.exports.join(', ')} } from './${fileName}'`
+        })
+      }
+    }
+  }
 }
