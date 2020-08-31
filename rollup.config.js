c514d2f0fe13ca389c075d03d93763594c4d87c7
William Swanson <swansontec@gmail.com>
2020-08-31 09:20:40 -0700

    Improve the build setup

--- rollup.config.js
+++ rollup.config.js
@@ -4,8 +4,11 @@ import packageJson from './package.json'
 
 export default {
   input: 'src/index.js',
-  output: { file: packageJson.main, format: 'cjs', sourcemap: true },
-  plugins: [mjs(), filesize()]
+  output: [
+    { file: packageJson.main, format: 'cjs', sourcemap: true },
+    { file: packageJson.module, format: 'esm', sourcemap: true }
+  ],
+  plugins: [filesize(), mjs()]
 }
 
 /**
@@ -15,13 +18,14 @@ function mjs() {
   return {
     name: 'rollup-plugin-mjs',
     generateBundle(options, bundle) {
+      if (options.format !== 'cjs') return
       for (const fileName of Object.keys(bundle)) {
         const chunk = bundle[fileName]
         if (chunk.type !== 'chunk' || !chunk.isEntry) continue
         const names = chunk.exports.join(',\n  ')
         this.emitFile({
           type: 'asset',
-          fileName: fileName.replace(/js$/, 'mjs'),
+          fileName: fileName.replace(/\.js$/, '') + '.mjs',
           source: `import cjs from './${fileName}';\n\nexport const {\n  ${names}\n} = cjs;\n`
         })
       }
