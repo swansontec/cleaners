a9398cd95af75ba403a656bf5c295a420de36acd
William Swanson <swansontec@gmail.com>
2021-02-10 08:07:11 -0800

    Stop emitting bundle sourcemaps

--- rollup.config.js
+++ rollup.config.js
@@ -5,8 +5,8 @@ import packageJson from './package.json'
 export default {
   input: 'src/index.js',
   output: [
-    { file: packageJson.main, format: 'cjs', sourcemap: true },
-    { file: packageJson.module, format: 'esm', sourcemap: true }
+    { file: packageJson.main, format: 'cjs' },
+    { file: packageJson.module, format: 'esm' }
   ],
   plugins: [filesize(), mjs()]
 }
