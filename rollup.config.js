855eb821c6d88a92b28dbbaa20659efb7800f18c
William Swanson <swansontec@gmail.com>
2021-02-24 08:20:50 -0800

    Switch to rollup-plugin-mjs-entry for node support

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import filesize from 'rollup-plugin-filesize'
+import mjs from 'rollup-plugin-mjs-entry'
 
 import packageJson from './package.json'
 
@@ -6,26 +7,4 @@ export default {
   input: './src/index.js',
   output: { file: packageJson.main, format: 'cjs' },
   plugins: [filesize(), mjs()]
-}
-
-/**
- * Tiny plugin to generate .mjs wrappers for each entry point.
- */
-function mjs() {
-  return {
-    name: 'rollup-plugin-mjs',
-    generateBundle(options, bundle) {
-      if (options.format !== 'cjs') return
-      for (const fileName of Object.keys(bundle)) {
-        const chunk = bundle[fileName]
-        if (chunk.type !== 'chunk' || !chunk.isEntry) continue
-        const names = chunk.exports.join(',\n  ')
-        this.emitFile({
-          type: 'asset',
-          fileName: fileName.replace(/\.js$/, '') + '.mjs',
-          source: `import cjs from './${fileName}';\n\nexport const {\n  ${names}\n} = cjs;\n`
-        })
-      }
-    }
-  }
 }
