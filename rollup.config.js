6ed133861b8c654cb026422663611e262021e204
William Swanson <swansontec@gmail.com>
2021-03-12 13:16:25 -0800

    Use Babel to enable modern syntax

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,20 @@
+import babel from '@rollup/plugin-babel'
 import filesize from 'rollup-plugin-filesize'
 import mjs from 'rollup-plugin-mjs-entry'
 
 import packageJson from './package.json'
 
+const babelOpts = {
+  babelrc: false,
+  presets: [['@babel/preset-env', { loose: true }]],
+  plugins: [['@babel/plugin-transform-for-of', { assumeArray: true }]]
+}
+
 export default {
   input: './src/index.js',
-  output: { file: packageJson.main, format: 'cjs' },
-  plugins: [filesize(), mjs()]
+  output: [
+    { file: packageJson.main, format: 'cjs' },
+    { file: packageJson.module, format: 'es' }
+  ],
+  plugins: [babel(babelOpts), filesize(), mjs()]
 }
