92c0f677daac1bfba8156b869db83492f47ff0cf
William Swanson <swansontec@gmail.com>
2023-03-23 02:02:40 -0700

    Use a global flag for uncleaning
    Even if multiple copies of cleaners exist in node_modules, they can all agree on the mode now.

--- src/cleaners/asCodec.js
+++ src/cleaners/asCodec.js
@@ -1,18 +1,35 @@
-let uncleaning = 0
-
 export function asCodec(cleaner, uncleaner) {
   return function asCodec(raw) {
-    return uncleaning > 0 ? uncleaner(raw) : cleaner(raw)
+    return global[uncleaning] > 0 ? uncleaner(raw) : cleaner(raw)
   }
 }
 
 export function uncleaner(cleaner) {
   return function uncleaner(raw) {
+    // We need to set a flag to indicate that we are un-cleaning.
+    // The flag needs to be in a global location,
+    // since multiple cleaner implementations should agree
+    // which mode we are in.
+    if (global[uncleaning] == null) {
+      Object.defineProperty(global, uncleaning, {
+        value: 0,
+        writable: true
+      })
+    }
     try {
-      ++uncleaning
+      ++global[uncleaning]
       return cleaner(raw)
     } finally {
-      --uncleaning
+      --global[uncleaning]
     }
   }
 }
+
+// If we cannot find the standardized "globalThis" object,
+// fall back on using the "JSON" constructor, which is also well-known.
+const global =
+  typeof globalThis === 'object' && globalThis != null ? globalThis : JSON
+
+/* istanbul ignore next */
+const uncleaning =
+  typeof Symbol === 'function' ? Symbol.for('uncleaning') : 'uncleaning'
